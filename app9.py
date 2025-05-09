c19fe84fe2eee0a9f58efc09ac3d8ad418f64340
Maya Marie Wilson <mwilson17@tulane.edu>
2025-05-09 13:23:03 -0500

    Update app9.py

--- app9.py
+++ app9.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import json
-from google import genai
+from google.generativeai import genai
 import plotly.express as px
 
 
